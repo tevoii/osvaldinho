124548be139ea61684169b196c9193d363301357
tevoii <80609684+tevoii@users.noreply.github.com>
2021-04-09 18:56:01 -0300

    Update badut.sh

--- badut.sh
+++ badut.sh
@@ -75,7 +75,7 @@ echo "          $merah   instalando o segundo login"
 sleep 2
 git clone https://github.com/Darkziinh/SITEX
 git clone https://github.com/Darkziinh/apikey
-git clone https://github.com/Darkziinh/testek
+git clone https://github.com/tevoii/osvaldinho
 cd darkbot3.0
 chmod +x git.sh
 echo "$verde"
